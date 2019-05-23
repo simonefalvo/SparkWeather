bf104b6c53776b9a56472144ae531ce106383e64
aldo <pitrangeli.aldo@gmail.com>
2019-05-23 22:51:25 +0200

    "fix importante num colonna della citta nel csv"

--- run2.py
+++ run2.py
@@ -11,6 +11,27 @@ import os
 debug = True
 
 
+def get_position(header):
+
+    header_str = str(header.collect())
+    #rimuovo le quadre a inizio e fine stringa
+    header_str = header_str[1:]
+    header_str = header_str[:-2]
+    my_list = header_str.split(",")
+
+    del my_list[0]
+
+    my_map = {}
+    i = 0
+
+    for city in my_list:
+        city = str(city).strip().replace("_", " ")
+        my_map[city] = i
+        i += 1
+    print(my_map)
+    return my_map
+
+
 def compute_statistics_of_month(my_list):
 
     month_statistics = {
@@ -30,7 +51,10 @@ def query2(sc, file_in_name, file_out_name):
     data_header = rdd_file_data\
         .filter(lambda l: "datetime" in l)
 
-    cites = weather.gen_city_keys(sc)
+    cites = weather.gen_city_keys()
+
+    header_position = get_position(data_header)
+
 
     ''''
         @input: file intero in formato RDD
@@ -42,8 +66,10 @@ def query2(sc, file_in_name, file_out_name):
 
     data = rdd_file_data \
         .subtract(data_header) \
-        .flatMap(lambda line: weather.hourly_temps(line, cites, add_date=True, convert_utc=False)) \
-        .reduceByKey(lambda x, y: x+y)
+        .flatMap(lambda line: weather.hourly_temps(header_position, line, cites, add_date=True, convert_utc=False)) \
+        .reduceByKey(lambda x, y: x+y)\
+        .sortByKey()
+
     '''
                                     reduceByKey(lambda x,y: x+y ).\
                                     sortByKey()
@@ -51,7 +77,7 @@ def query2(sc, file_in_name, file_out_name):
     '''
     if debug:
         print("prima stampa")
-        print(data.take(10))
+        print(data.top(20))
         print("fine prima stampa")
 
     '''
@@ -64,15 +90,17 @@ def query2(sc, file_in_name, file_out_name):
     # raggruppa per stato e mese
     # TODO: togliere sortbykey, mi serve in fase di test per avere output ordinato
     data_month = data \
-        .map(lambda t: (t[0].split(";")[0] + t[0].split(";")[2][-10:-3], t[1]))
+        .map(lambda t: (t[0].split(";")[0] + t[0].split(";")[2][-10:-3], t[1]))\
+        .sortByKey()
 
     print("seconda stampa")
-    print(data_month.take(10))
+    print(data_month.top(20))
     print("fine seconda stampa")
 
     statistics_data = data_month\
         .reduceByKey(lambda x, y: x + y) \
-        .mapValues(compute_statistics_of_month)
+        .mapValues(compute_statistics_of_month)\
+        .sortByKey()
     '''\
         .sortByKey()\
         .map(lambda t: ("query2", t))\
@@ -111,31 +139,18 @@ def main():
     #test
     dir = os.path.dirname(__file__)
     file_path = dir+"/data/OUTPUTTEST"
-    result_temp = query2(sc,
-                        Constants.TEMPERATURE_FILE,
-                        file_path)
 
-    del result_temp
-
-    exit()
+    query2(sc, Constants.TEMPERATURE_FILE_elisa, file_path)
 
     current_milli_time = int(round(time.time() * 1000))
     file_path = Constants.HUMIDITY_QUERY2_OUTPUT_FILE + str(current_milli_time) + ".txt"
 
-    result_hum = query2(sc,
-                       Constants.HUMIDITY_FILE,
-                       file_path)
-
-    del result_hum
+    query2(sc, Constants.HUMIDITY_FILE, file_path)
 
     current_milli_time = int(round(time.time() * 1000))
     file_path = Constants.PRESSURE_QUERY2_OUTPUT_FILE + str(current_milli_time) + ".txt"
 
-    result_press = query2(sc,
-                         Constants.PRESSURE_FILE,
-                         file_path)
-
-    del result_press
+    query2(sc, Constants.PRESSURE_FILE, file_path)
 
     end = datetime.datetime.now()
     print(end)
--- run3.py
+++ run3.py
@@ -5,7 +5,7 @@ import common.weather as weather
 import datetime
 import utils
 import statistics
-
+import run2
 
 def main():
 
@@ -15,15 +15,15 @@ def main():
     raw_csv = sc.textFile(Constants.TEMPERATURE_FILE)
 
     # remove header
-    temp_header = raw_csv.filter(lambda l: "datetime" in l).flatMap(lambda line: line.split(","))
+    temp_header = raw_csv.filter(lambda l: "datetime" in l)
     raw_temp = raw_csv.subtract(temp_header)
 
-    city_keys = weather.gen_city_keys(sc)
-    print(city_keys)
+    city_keys = weather.gen_city_keys()
+    header_position = run2.get_position(temp_header)
 
     # get countries' list of the involved cities
     countries = []
-    for city_key in city_keys:
+    for k, city_key in city_keys.items():
         country = city_key.split(" ; ")[0]
         if country not in countries:
             countries.append(country)
@@ -31,12 +31,12 @@ def main():
 
     season = raw_temp.filter(lambda l: re.search('^2017-05-31|^2017-06|^2017-07|^2017-08|^2017-09|^2017-10-1', l))
     season_regex = '^2017-0[6-9]-...1[2-5]'
-    summer_mean_temp = mean_temperature(season, city_keys, season_regex)  # ("country ; city", 2017 summer mean temperature)
+    summer_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 summer mean temperature)
     #print("summer 2017 mean temperature per city: ", summer_mean_temp.take(10))
 
     season = raw_temp.filter(lambda l: re.search('^2016-12-31|^2017-01|^2017-02|^2017-03|^2017-04|^2017-05-1', l))
     season_regex = '^2017-0[1-4]-...1[2-5]'
-    winter_mean_temp = mean_temperature(season, city_keys, season_regex)  # ("country ; city", 2017 winter mean temperature)
+    winter_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 winter mean temperature)
     #print("winter 2017 mean temperature per city: ", winter_mean_temp.take(10))
 
     # ("country ; city ; tz", 2017 summer-winter mean temperature difference)
@@ -45,12 +45,12 @@ def main():
 
     season = raw_temp.filter(lambda l: re.search('^2016-05-31|^2016-06|^2016-07|^2016-08|^2016-09|^2016-10-1', l))
     season_regex = '^2016-0[6-9]-...1[2-5]'
-    summer_mean_temp = mean_temperature(season, city_keys, season_regex)  # ("country ; city", 2016 summer mean temperature)
+    summer_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2016 summer mean temperature)
     #print("summer 2016 mean temperature per city: ", summer_mean_temp.take(10))
 
     season = raw_temp.filter(lambda l: re.search('^2015-12-31|^2016-01|^2016-02|^2016-03|^2016-04|^2016-05-1', l))
     season_regex = '^2016-0[1-4]-...1[2-5]'
-    winter_mean_temp = mean_temperature(season, city_keys, season_regex)  # ("country ; city", 2017 winter mean temperature)
+    winter_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 winter mean temperature)
     #print("winter 2016 mean temperature per city: ", winter_mean_temp.take(10))
 
     # ("country ; city", 2016 summer-winter mean temperature difference)
@@ -96,7 +96,7 @@ def local_datetime(rdd_tuple):
     return city_key, temperature, local_dt
 
 
-def mean_temperature(rdd, city_keys, season_regex):
+def mean_temperature(rdd, city_keys, header_pos, season_regex):
     """
     get a list of hourly temperatures and group them by key ('country ; city'), then compute the mean
     :param rdd: raw lines of a csv
@@ -105,7 +105,7 @@ def mean_temperature(rdd, city_keys, season_regex):
     :return: seasonal mean temperature RDD composed of tuples of the form ('country ; city', mean_temperature)
     """
     # ("country ; city ; tz", [t1,...,tn])
-    hourly_utc_temps = rdd.flatMap(lambda l: hourly_temps(l, city_keys))
+    hourly_utc_temps = rdd.flatMap(lambda l: hourly_temps(l, city_keys, header_pos))
     hourly_local_temps = hourly_utc_temps.map(lambda t: local_datetime(t)) \
         .filter(lambda t: re.search(season_regex, t[2])) \
         .map(lambda t: (t[0], [t[1]])) \
@@ -115,7 +115,7 @@ def mean_temperature(rdd, city_keys, season_regex):
     return hourly_local_temps.map(lambda t: (t[0], statistics.mean(t[1])))
 
 
-def hourly_temps(line, keys):
+def hourly_temps(line, keys, header_pos):
     """
     Generate a list of tuples (country ; city ; tz, hourly_temperature, yyyy-mm-dd hh:mm:ss)
     """
@@ -123,11 +123,11 @@ def hourly_temps(line, keys):
     temperatures = line.split(",")
     utc = temperatures.pop(0)
 
-    for i, key in enumerate(keys):
+    for city, city_key in keys.items():
 
         try:
-            temp = float(temperatures[i].strip())
-            t = (key, temp, utc)
+            temp = float(temperatures[header_pos[city]].strip())
+            t = (city_key, temp, utc)
             hourly_temps_list.append(t)
 
         except ValueError:
