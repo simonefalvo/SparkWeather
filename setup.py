16b0c01f111aeae6b265f2eff210aa2d8376fb8a
smvfal <smvfal@gmail.com>
2019-05-24 23:08:33 +0200

    container paths template nifi, project requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SparkWeather',
+    version='',
+    packages=['py4j', 'pyspark', 'pyspark.ml', 'pyspark.ml.param', 'pyspark.ml.linalg', 'pyspark.sql', 'pyspark.mllib',
+              'pyspark.mllib.stat', 'pyspark.mllib.linalg', 'pyspark.streaming'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
