15fdf532ae674218cd2389985c7b5466691b0dbf
smvfal <smvfal@gmail.com>
2019-05-24 19:32:16 +0200

    refactoring

--- run3.py
+++ run3.py
@@ -9,6 +9,7 @@ import statistics
 import run2
 import time
 
+
 def main():
 
     start = datetime.datetime.now()
@@ -31,13 +32,13 @@ def main():
             countries.append(country)
 
     season = raw_temp.filter(lambda l: re.search('^2017-05-31|^2017-06|^2017-07|^2017-08|^2017-09|^2017-10-1', l))
-    season_regex = '^2017-0[6-9]-...1[2-5]'
-    summer_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 summer mean temperature)
+    # ("country ; city", 2017 summer mean temperature)
+    summer_mean_temp = mean_temperature(season, city_keys, header_position, '^2017-0[6-9]-...1[2-5]')
     #print("summer 2017 mean temperature per city: ", summer_mean_temp.take(10))
 
     season = raw_temp.filter(lambda l: re.search('^2016-12-31|^2017-01|^2017-02|^2017-03|^2017-04|^2017-05-1', l))
-    season_regex = '^2017-0[1-4]-...1[2-5]'
-    winter_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 winter mean temperature)
+    # ("country ; city", 2017 winter mean temperature)
+    winter_mean_temp = mean_temperature(season, city_keys, header_position, '^2017-0[1-4]-...1[2-5]')
     #print("winter 2017 mean temperature per city: ", winter_mean_temp.take(10))
 
     # ("country ; city ; tz", 2017 summer-winter mean temperature difference)
@@ -45,20 +46,18 @@ def main():
     #print("2017 temperature difference per city: ", temp_diff.take(10))
 
     season = raw_temp.filter(lambda l: re.search('^2016-05-31|^2016-06|^2016-07|^2016-08|^2016-09|^2016-10-1', l))
-    season_regex = '^2016-0[6-9]-...1[2-5]'
-    summer_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2016 summer mean temperature)
+    # ("country ; city", 2016 summer mean temperature)
+    summer_mean_temp = mean_temperature(season, city_keys, header_position, '^2016-0[6-9]-...1[2-5]')
     #print("summer 2016 mean temperature per city: ", summer_mean_temp.take(10))
 
     season = raw_temp.filter(lambda l: re.search('^2015-12-31|^2016-01|^2016-02|^2016-03|^2016-04|^2016-05-1', l))
-    season_regex = '^2016-0[1-4]-...1[2-5]'
-    winter_mean_temp = mean_temperature(season, city_keys, header_position, season_regex)  # ("country ; city", 2017 winter mean temperature)
+    # ("country ; city", 2017 winter mean temperature)
+    winter_mean_temp = mean_temperature(season, city_keys, header_position, '^2016-0[1-4]-...1[2-5]')
     #print("winter 2016 mean temperature per city: ", winter_mean_temp.take(10))
 
     # ("country ; city", 2016 summer-winter mean temperature difference)
     prev_temp_diff = summer_mean_temp.join(winter_mean_temp).mapValues(lambda temps: abs(temps[0] - temps[1])).cache()
     #print("2016 temperature difference per city: ", prev_temp_diff.take(10))
-
-    list_save = []
 
     current_milli_time = int(round(time.time() * 1000))
 
@@ -72,15 +71,12 @@ def main():
             .map(lambda x: x[0]) \
             .zipWithIndex()
 
-
+        # TODO: ridurre l'rdd ai soli primi 15 elementi ordinati per alleggerire la join
         # ("country ; city", (temp, 2016 position))
         temp_chart_rdd = country_temp.join(prev_country_temp).sortBy(lambda x: -x[1][0]).cache()
+        temp_chart = temp_chart_rdd.takeOrdered(3, lambda x: -x[1][0])
 
-        #temp_chart = temp_chart_.takeOrdered(3, lambda x: -x[1][0])
-        temp_chart_collection = temp_chart_rdd.takeOrdered(3, lambda x: -x[1][0])
-        # TODO: ridurre l'rdd ai soli primi 15 elementi ordinati per alleggerire la join
-
-        for i, x in enumerate(temp_chart_collection):
+        for i, x in enumerate(temp_chart):
             print("{}-{}: {}, (2016 position: {})"
                   .format(i + 1,
                           x[0].split(" ; ")[1],
